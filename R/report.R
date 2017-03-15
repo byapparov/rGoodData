9c1f352bfb1a0c76b222e8cb2e594e3ba5e5f8f4
byapparov <byapparov@gmail.com>
2017-03-15 22:38:14 +0000

    Added tests for error codes and test recursive access to the report data if 201 is returned.

--- R/report.R
+++ R/report.R
@@ -2,8 +2,8 @@ library(httr)
 library(data.table)
 
 # Environment variables required:
-# GOODDATA_DOMAIN
-# GOODDATA_PROJECT
+# GOODDATA_DOMAIN - this is what goes before /gdc/ in your GoodData account
+# GOODDATA_PROJECT - this is the hash of the project that can be found in the url
 # GOODDATA_USER
 # GOODDATA_PASSWORD
 
@@ -29,7 +29,8 @@ getReportRawUri <- function(obj) {
                    content_type_json() ,
                    add_headers(Accept = "application/json",
                                "Content-Type" = "application/json"))
-  c <- content(response, "parsed", "application/json")
+
+  c <- processResponse(response)
   uri <- paste0(Sys.getenv("GOODDATA_DOMAIN"), c$uri)
   return(uri)
 }
@@ -46,8 +47,9 @@ getReportData <- function(uri, wait = 5) {
                   authenticate(Sys.getenv("GOODDATA_USER"), Sys.getenv("GOODDATA_PASSWORD")),
                   add_headers(Accept = "application/json",
                               "Content-Type" = "application/json"))
-  if(status_code(response) >= 200 & status_code(response) < 300) {
-    if(response$headers$`content-type` == "text/csv") {
+  status <- status_code(response)
+  if(status >= 200 & status < 300) {
+    if(getResponseType(response) == "text/csv") {
       report <- content(response, "text", encoding = "UTF-8")
       report <- fread(report, encoding = "UTF-8")
     } else { # we have to retry as report data was not generated yet.
@@ -61,10 +63,10 @@ getReportData <- function(uri, wait = 5) {
     type <- http_type(response)
     if (type == "application/json") {
       out <- content(response, "parsed", "application/json")
-      stop("HTTP error [", out$error$errorClass, "] ", out$message, call. = FALSE)
+      stop("HTTP error [", out$error$code, "] ", out$message, call. = FALSE)
     } else {
       out <- content(response, "text")
-      stop("HTTP error [", response$status, "] ", out, call. = FALSE)
+      stop("HTTP error [", status, "] ", out, call. = FALSE)
     }
   }
 }
@@ -79,8 +81,32 @@ getLastDefinition <- function(report.obj) {
                   authenticate(Sys.getenv("GOODDATA_USER"), Sys.getenv("GOODDATA_PASSWORD")),
                   add_headers("Accept" = "application/json",
                               "Content-Type" = "application/json"))
-  c <- content(response, "parsed", "application/json")
+
+  c <- processResponse(response)
   last.definition <- tail(c$report$content$definitions, 1)[[1]]
   definition.obj <- tail(strsplit(last.definition, "/")[[1]], 1)
   return(as.integer(definition.obj))
+}
+
+#' Processes http response and returns a list of results
+#'
+#' @param response http response from the GoodData API
+#' @return list of resutls parsed from the response
+processResponse <- function(response) {
+  if(status_code(response) == 200) {
+    c <- content(response, "parsed", http_type(response))
+  } else { # error
+    type <- http_type(response)
+    if (type == "application/json") {
+      out <- content(response, "parsed", "application/json")
+      stop("HTTP error [", out$error$code, "] ", out$error$message, call. = FALSE)
+    } else {
+      out <- content(response, "text")
+      stop("HTTP error [", response$status, "] ", out, call. = FALSE)
+    }
+  }
+}
+
+getResponseType <-function(response) {
+  return(response$headers$`content-type`)
 }
--- tests/testthat/test.report.export.R
+++ tests/testthat/test.report.export.R
@@ -1,5 +1,6 @@
 library(httptest)
 
+context("Report data")
 with_mock_API({
   test_that("Report definition is extracted correctly", {
 
@@ -19,3 +20,33 @@ with_mock_API({
   })
 
 })
+
+context("Error Codes")
+library(mockery)
+
+with_mock(
+  status_code = mock(401, cycle = T),
+  buildMockURL = mock("test.com/401.json", cycle = T),
+  {
+    with_mock_API({
+      expect_error(res <- getLastDefinition(810164), regex = "401")
+      expect_error(res <- getReportRawUri(810164), regex = "401")
+      expect_error(res <- getReportData("test.com/dummy"), regex = "401")
+    })
+  }
+)
+
+with_mock(
+  status_code = mock(201, 201, 200),
+  buildMockURL = mock("test.com/report-data-201.json",
+                      "test.com/report-data-201.json",
+                      "test.com/report-data-200.csv"),
+  getResponseType = mock("application/json", "application/json", "text/csv"),
+  {
+    with_mock_API({
+       expect_output(res <- getReportData("test.com/dummy", 0.012), regexp = "0.012")
+       expect_true(is.data.table(res))
+       expect_identical(nrow(res), 2L)
+    })
+  }
+)
