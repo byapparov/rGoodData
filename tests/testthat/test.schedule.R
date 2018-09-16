61340d1788869102e69363f9c3e06ecac410438c
Bulat Yapparov <byapparov@gmail.com>
2018-09-16 13:51:31 +0100

    gdLoadReport (#9)
    * Added function that allows to start GoodData schedule by ID.
    
    * Fixed tests for httptest update.
    
    * Updated packrat lock
    
    * Adding gdLoadReport function for easy report data uploads
    
    * Adding documentation for gdLoadReport
    
    * Added .Rbuildignore to git

--- tests/testthat/test.schedule.R
+++ tests/testthat/test.schedule.R
@@ -6,6 +6,11 @@ context("Schedule")
 Sys.setenv(GOODDATA_DOMAIN = "test.com")
 Sys.setenv(GOODDATA_PROJECT = "dummy")
 
+
+test_that("Schedule execution sends POST request" , {
+  without_internet(expect_POST(schedule.uri <- executeSchedule("abcd")))
+})
+
 with_mock(authCookie = mock("", cycle = F), {
   with_mock_API({
     test_that("Schedule execution works" , {
