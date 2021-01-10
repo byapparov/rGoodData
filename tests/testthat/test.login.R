13c1e237be177ce2041adcdef877fb817fcddba5
Neal Richardson <neal.p.richardson@gmail.com>
2021-01-10 04:16:37 -0800

    Replace httptest::with_mock_API with with_mock_api (#10)
    Hi @nealrichardson,
    
    Thank you for this!
    
    Bulat

--- tests/testthat/test.login.R
+++ tests/testthat/test.login.R
@@ -4,7 +4,7 @@ library(mockery)
 context("Login")
 
 test_that("Login procedure works", {
-  with_mock_API({
+  with_mock_api({
     expect_POST(sst <- superSecuredToken())
     expect_GET(sst <- temporaryToken("dummy"))
   })
@@ -14,8 +14,8 @@ test_that("Login procedure works", {
 test_that("Login unauthorized message is correct", {
   with_mock(
     status_code = mock(401L, cycle = T),
-    buildMockURL = mock("test.com/login-401.json", cycle = T),
-    with_mock_API({
+    build_mock_url = mock("test.com/login-401.json", cycle = T),
+    with_mock_api({
       expect_error(sst <- superSecuredToken(), regexp = "401")
     })
   )
@@ -26,7 +26,7 @@ Sys.setenv(GOODDATA_DOMAIN = "test.com")
 Sys.setenv(GOODDATA_PROJECT = "dummy")
 
 test_that("Correct auth code is captured from response header", {
-  with_mock_API({
+  with_mock_api({
     Sys.setenv(TZ='UTC')
     sst <- superSecuredToken()
     expect_match(sst, "^GDCAuthTT=")
